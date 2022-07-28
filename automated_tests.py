ef18aa0f6848c2da813d5e2a4d6b4b6a834b8ac5
William Silversmith <william.silversmith@gmail.com>
2022-07-27 21:29:52 -0400

    test: add test for fixed rate mode

--- automated_tests.py
+++ automated_tests.py
@@ -6,11 +6,10 @@ import pytest
 def vector_img():
 	return np.load("vector_field_sample.npy")
 
-@pytest.mark.parametrize("correlated_dims", [[True,True,True,True], [True,True,False,False]])
+@pytest.mark.parametrize("correlated_dims", [[True,True,True,True], [True,True,False,False], [False,True,True,True]])
 @pytest.mark.parametrize("order", ['C', 'F'])
-def test_compression_decompression(vector_img, correlated_dims, order):
-	tolerance = 0.01
-
+@pytest.mark.parametrize("tolerance", [ 0.000002, 0.0001, 0.01 ])
+def test_compression_decompression_fixed_accuracy(vector_img, correlated_dims, order, tolerance):
 	if order == "C":
 		vector_img = np.ascontiguousarray(vector_img)
 	else:
@@ -23,7 +22,25 @@ def test_compression_decompression(vector_img, correlated_dims, order):
 	)
 	recovered = zfpc.decompress(compressed)
 
+	print(f"compression ratio: {len(compressed)/vector_img.nbytes*100:.3f}%")
+
 	assert np.allclose(recovered, vector_img, atol=tolerance)
 
+@pytest.mark.parametrize("correlated_dims", [[True,True,True,True], [True,True,False,False], [False,True,True,True]])
+@pytest.mark.parametrize("order", ['C', 'F'])
+@pytest.mark.parametrize("rate", [ 8, 10, 12 ])
+def test_compression_decompression_fixed_rate(vector_img, correlated_dims, order, rate):
+	if order == "C":
+		vector_img = np.ascontiguousarray(vector_img)
+	else:
+		vector_img = np.asfortranarray(vector_img)
 
+	compressed = zfpc.compress(
+		vector_img, 
+		rate=rate, 
+		correlated_dims=correlated_dims
+	)
+	recovered = zfpc.decompress(compressed)
+	print(len(compressed) / vector_img.nbytes)
+	print(np.max(np.abs(recovered - vector_img)))
 
